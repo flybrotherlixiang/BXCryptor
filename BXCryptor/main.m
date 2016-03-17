4a1a400f637e4888baef5200457bd11a09af2964
Xiang Li <lixiang@baixing.com>
2016-03-17 11:00:59 +0800

    Initial Commit

---
+++ BXCryptor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BXCryptor
+//
+//  Created by 李翔 on 3/17/16.
+//  Copyright © 2016 Xiang Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
